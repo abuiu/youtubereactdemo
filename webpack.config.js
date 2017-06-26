727a8691e9586182f1d5737799776f67dcd86559
Andrei Buiu <andrei@buiu.co.uk>
2017-06-26 12:59:30 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    entry: ['./main.js'],
+
+    output: {
+        path: __dirname,
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        inline: true,
+        port: 1337
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['react','es2015', 'stage-1']
+            }
+
+        }]
+    }
+}
